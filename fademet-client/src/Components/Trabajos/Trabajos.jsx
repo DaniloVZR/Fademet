d94705a1f3d0193dfdb406ae9f4cce539be986a3
Danilo <daniloud2809@gmail.com>
2023-01-19 21:35:28 -0500

    Sección trabajos casi finalizada

--- fademet-client/src/Components/Trabajos/Trabajos.jsx
+++ fademet-client/src/Components/Trabajos/Trabajos.jsx
@@ -1,11 +1,85 @@
 import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+import images from "../../utils/images";
+import "./Trabajos.css"
+import { useState, useEffect } from "react";
 
 const Trabajos = () => {
+
+  const [showImage, setShowImage] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleNext = () => {
+    const currentIndex = images.findIndex(image => image.id === selectedImage.id);
+    if (currentIndex === images.length - 1) return;
+    setSelectedImage(images[currentIndex + 1]);
+  }
+
+  const handlePrev = () => {
+    const currentIndex = images.findIndex(image => image.id === selectedImage.id);
+    if (currentIndex === 0) return;
+    setSelectedImage(images[currentIndex - 1]);
+  }
+
+  const handlerImageShow = (image) => {
+    setSelectedImage(image)
+    setShowImage(true);
+  }
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+    setShowImage(false);
+  }
+
+  const handleClickOutside = (e) => {
+    if (!e.target.closest('.image-selected')) {
+      handleCloseModal();
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+  }, []);
+
   return (
-    <div className="Trabajos">
+    <div className="trabajos">
       <Navbar />
-      Trabajos
-    </div>
+      <div className="trabajos-wrapper">
+        <div className="trabajos-container">
+          <div className="trabajos-title">
+            <h1>Trabajos</h1>
+            <h3>Estos fueron algunos de los trabajos realizados:</h3>
+          </div>
+          {
+            images.length ? <div className="trabajos-gallery">
+              {
+                images.map((image) => {
+                  return (
+                    <div className="trabajos-gallery__container" key={image.id} onClick={() => handlerImageShow(image)}>
+                      <img className="trabajos-gallery__picture" src={image.photo} alt={image.title} />
+                    </div>
+                  )
+                })}
+            </div> : <div> <i className="fa-solid fa-circle-notch fa-3x fa-spin"></i> </div>
+          }
+        </div>
+        {
+          showImage &&
+          <div className="image-selected">
+            <button onClick={handleCloseModal} className="image-selected__btn">X</button>
+            <img src={selectedImage.photo} alt={selectedImage.title} />
+            <div className="image-selected__btn__flex">
+              <button onClick={handlePrev} className="image-selected__btn">←</button>
+              <button onClick={handleNext} className="image-selected__btn">→</button>
+            </div>
+          </div>
+        }
+      </div>
+      <Footer />
+    </div >
   )
 }
 
---
+++ fademet-client/src/utils/images.js
@@ -0,0 +1,58 @@
+  const images = [
+    {
+      "id" : 1,
+      "photo": require("../img/Gallery/1.jpg"),
+      "title": "Imagen 1",
+      "description" : "Esta es una descripción de la imagen 1",
+    },
+    {
+      "id" : 2,
+      "photo": require("../img/Gallery/2.jpg"),
+      "title": "Imagen 2",
+      "description" : "Esta es una descripción de la imagen 2",
+    },
+    {
+      "id" : 3,
+      "photo": require("../img/Gallery/3.jpg"),
+      "title": "Imagen 3",
+      "description" : "Esta es una descripción de la imagen 3",
+    },
+    {
+      "id" : 4,
+      "photo": require("../img/Gallery/4.jpg"),
+      "title": "Imagen 4",
+      "description" : "Esta es una descripción de la imagen 4",
+    },
+    {
+      "id" : 5,
+      "photo": require("../img/Gallery/5.jpg"),
+      "title": "Imagen 5",
+      "description" : "Esta es una descripción de la imagen 5",
+    },
+    {
+      "id" : 6,
+      "photo": require("../img/Gallery/6.jpg"),
+      "title": "Imagen 6",
+      "description" : "Esta es una descripción de la imagen 6",
+    },
+    {
+      "id" : 7,
+      "photo": require("../img/Gallery/7.jpg"),
+      "title": "Imagen 7",
+      "description" : "Esta es una descripción de la imagen 7",
+    },
+    {
+      "id" : 8,
+      "photo": require("../img/Gallery/8.jpg"),
+      "title": "Imagen 8",
+      "description" : "Esta es una descripción de la imagen 8",
+    },
+    {
+      "id" : 9,
+      "photo": require("../img/Gallery/9.jpg"),
+      "title": "Imagen 9",
+      "description" : "Esta es una descripción de la imagen 9",
+    },
+  ]
+
+  export default images;
