558a107b81d0ff195f2ed28794d07b4d6951593f
Danilo Urrego <daniloud2809@gmail.com>
2023-03-01 22:11:14 -0500

    New Home

--- fademet-client/src/Components/Inicio/Description/Description.jsx
+++ fademet-client/src/Components/Inicio/Description/Description.jsx
@@ -1,13 +1,22 @@
 import "./Description.css";
+import { Link } from "react-router-dom";
 import Logo from "../../../img/logo_fm.png";
 
 const Description = () => {
   return (
-    <div class="desc">
-      <div className="desc__logo">
-        <img className="desc__logo-img" src={Logo} alt="Logo Fademet" />
-        <div>
-          <h5>Somos una empresa domiciliada y creada en la ciudad de Medellín, con el fin de ofrecer productos y servicios metalmecánicos, estamos especializados en el diseño, fabricación y montaje de estructuras metálicas para el sector industrial y doméstico.</h5>
+    <div className="description">
+      <div className="desc__shape" />
+      <div class="desc">
+        <div className="desc__info">
+          <div class="card">
+            <div class="card-info">
+              <img className="desc__info-logo" src={Logo} alt="Logo Fademet" />
+              <h6 class="title">Somos una empresa domiciliada y creada en la ciudad de Medellín, con el fin de ofrecer productos y servicios metalmecánicos, estamos especializados en el diseño, fabricación y montaje de estructuras metálicas para el sector industrial y doméstico.</h6>
+            </div>
+          </div>
+          <Link to='/'>
+            <button className="button btn-width">Servicios</button>
+          </Link>
         </div>
       </div>
     </div>
