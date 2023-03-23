761bdd47cc202e3109dc133c62f5e7fe75314b2f
Danilo Urrego <daniloud2809@gmail.com>
2023-03-22 19:43:55 -0500

    update: rediseño de sección proyectos

--- fademet-client/src/Components/Servicios/Servicios.jsx
+++ fademet-client/src/Components/Servicios/Servicios.jsx
@@ -1,47 +1,31 @@
 import servicesList from "../../utils/Services";
-import { motion } from "framer-motion";
 import "./Servicios.css";
 
 const Servicios = () => {
-
-  const container = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: {
-        delayChildren: 0.5
-      }
-    }
-  }
-
-  const item = {
-    hidden: { opacity: 0, scale: 0 },
-    show: { opacity: 1, scale: 1 }
-  }
-
-
   return (
     <section className="servicios" id="servicios">
+      <div className="servicios__photo" />
       <div className="servicios-container">
         <div className="section-title">
           <h1>Nuestros Servicios</h1>
           <h3>En Fademet contamos con los siguientes servicios:</h3>
         </div>
-        <motion.div
-          variants={container}
-          initial="hidden"
-          animate="show"
-          className="servicios-content">
-          {
-            servicesList.map((service, i) => {
-              // const isFirstColumn = i < servicesList.length / 2
-              // const className = `servicio-option ${isFirstColumn ? 'column-1' : 'column-2'}`;
+        <div className="servicios-content">
+          <ul>
+            {servicesList.slice(0, Math.ceil(servicesList.length / 2)).map((service, i) => {
               return (
-                <motion.div key={i} className={`servicio-option`} variants={item}>{service}</motion.div>
+                <li key={i} className="servicio-option">{service}</li>
               )
-            })
-          }
-        </motion.div>
+            })}
+          </ul>
+          <ul>
+            {servicesList.slice(Math.ceil(servicesList.length / 2), servicesList.length).map((service, i) => {
+              return (
+                <li key={i} className="servicio-option">{service}</li>
+              )
+            })}
+          </ul>
+        </div>
       </div>
     </section>
   )
